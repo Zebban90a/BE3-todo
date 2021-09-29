b9b7de56ce0039484b78149382250abf38707d26
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-09-29 15:20:17 +0200

    Starting with react

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import React from "react"
+
+import AllLists from "./components/GetAllLists"
 
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+  return <AllLists />
 }
 
-export default App;
+export default App
