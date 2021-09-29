8654d740176fe710dc52ce968227d0d5b7b5d749
Sebastian Orbom <zebban90a@gmail.com>
2021-09-29 12:04:14 +0200

    database connected successfully

--- server/models/todoLists.js
+++ server/models/todoLists.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const NewTodoListSchema = new Schema({
-  titel: { type: string },
+  titel: { type: String },
+  todos: {type: Array}
 });
+
+const NewToDo = new Schema({
+  titel: { type: String },
+
+  
+});
+
 
 module.exports = mongoose.model("todoLists", NewTodoListSchema);
