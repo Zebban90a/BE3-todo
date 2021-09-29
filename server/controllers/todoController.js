eb1bcccd0ea9f00b5b4cefa95e167ccbf2004861
Sebastian Orbom <zebban90a@gmail.com>
2021-09-29 14:00:10 +0200

    get all todos

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,14 +1,9 @@
-
 const NewTodoListSchema = require('../models/todoLists')
 
-const todolists = NewTodoListSchema
-
 exports.getAllTodoLists = (req, res) => {
-    res.status(200).json({
-      status: 'success',
-      results: todolists.length,
-      data: {
-        data,
-      },
-    });
+    
+    res.status(200).json();
+    console.log(NewTodoListSchema)
   };
+
+
