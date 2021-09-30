b3ff2612afcb20e76e4ab65f176a170e22b456a6
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-09-30 14:25:42 +0200

    Made a post form and started working on client side fetch

---
+++ Client/src/components/PostForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+
+export default function PostForm() {
+  const [title, setTitle] = useState("")
+  const [text, setText] = useState("")
+
+  let changeHandler = (e) => {
+    if (e.target.name == "title") {
+      setTitle(e.target.value)
+    }
+  }
+
+  let submitHandler = (e) => {
+    e.preventDefault()
+    console.log(`This is the title ${title}`)
+    console.log(`This is the text ${text}`)
+  }
+
+  return (
+    <>
+      <h1>Skapa en ny todo lista</h1>
+      <div>
+        <form onSubmit={submitHandler} action="">
+          <input
+            name="title"
+            type="text"
+            placeholder="titel"
+            onChange={changeHandler}
+          />
+          <br />
+          {/* <textarea
+            name="text"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Skriv din todo"
+            onChange={changeHandler}
+          ></textarea> */}
+          <br />
+          <button type="submit">Spara</button>
+        </form>
+      </div>
+    </>
+  )
+}
