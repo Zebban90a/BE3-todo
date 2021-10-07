030ef2f36e9032181b6c6bfa8e3ddd3db90ae867
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-10-07 09:57:24 +0200

    Added a redirect if login fails

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,53 +1,48 @@
-const userModel = require('../models/user')
-const passport = require('passport')
-
+const userModel = require("../models/user")
+const passport = require("passport")
 
 exports.userLogin = (req, res, next) => {
-  console.log('dyssman')
+  console.log("dyssman")
   /*passport.authenticate('local', { successRedirect: '/',
                                    failureRedirect: '/login',
                                    failureFlash: true }) */
-  passport.authenticate('local', 
-  (err, user, info) => {
+  passport.authenticate("local", (err, user, info) => {
     if (err) {
-      return next(err);
+      return next(err)
     }
 
     if (!user) {
-      return res.status(404);
-    }
-
-    else {
+      return res.status(404).redirect("http://localhost:3000/login")
+    } else {
       console.log(user)
-      return res.redirect("http://localhost:3000/");
+      return res.redirect("http://localhost:3000/")
     }
-  
-  })(req, res, next); 
-  
-};
-
-exports.registerUser = async (req, res) => {
-      userModel.register(userModel({
-          username: req.body.username,
-          }),
-          req.body.password, function(err,user) {
-              //om error
-              if(err){
-                  console.log(err);
-                  res.send("register");
-              }
-              //annars
-              passport.authenticate("local")(req,res,function(){
-              res.redirect("/login");
-              })  
-          }) 
+  })(req, res, next)
 }
 
+exports.registerUser = async (req, res) => {
+  userModel.register(
+    userModel({
+      username: req.body.username,
+    }),
+    req.body.password,
+    function (err, user) {
+      //om error
+      if (err) {
+        console.log(err)
+        res.send("register")
+      }
+      //annars
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/login")
+      })
+    }
+  )
+}
 
 exports.getLoginpage = (req, res) => {
-
   res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined',
-  });
-};
+    status: "error",
+    message: "this route is not defined",
+  })
+}
