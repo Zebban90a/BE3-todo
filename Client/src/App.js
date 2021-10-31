d92ccbc9f1054f3a082b109c7057e94e7c37b997
Sebastian Orbom <zebban90a@gmail.com>
2021-10-31 16:20:19 +0100

    trying to solve redirect

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,27 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import AllListPage from "./pages/AllListPage"
 import ListPage from "./pages/ListPage"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 
 function App() {
+  const token = window.localStorage.getItem("token")
+  const history = useHistory();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function checkIfLoggedIn () {
+    if(token) {
+      return setIsLoggedIn(true);
+    } else {
+      return history.push('/login')
+    }
+  }
+
+  useEffect(() => {
+    console.log('hallolhll')
+    checkIfLoggedIn()
+  }, [])
+
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/:listId">
-            <ListPage />
-          </Route>
-          <Route path="/">
-            <AllListPage />
-          </Route>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/register" exact component={Register} />
+          {isLoggedIn && <Route path="/:listId" exact component ={ListPage} /> }
+          {isLoggedIn && <Route path="/" exact component = {AllListPage} /> }
         </Switch>
       </Router>
     </>
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,7 +1,14 @@
-import React from "react"
-
+import React, { useEffect } from "react"
+import { useLocation } from 'react-router-dom';
 import { LoginForm } from "../components/LoginForm"
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 export default function Login() {
+  const query = useQuery();
+  useEffect(() => {
+    console.log('tradad')
+  }, [query])
   return <LoginForm />
 }
