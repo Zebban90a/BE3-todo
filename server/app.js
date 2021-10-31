8cf499a592c28f2ca680513bbb2a1b81cb422e85
Sebastian Orbom <zebban90a@gmail.com>
2021-10-31 23:56:14 +0100

    corss

--- server/app.js
+++ server/app.js
@@ -28,9 +28,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
-app.options('*', cors())
 app.use("/api/todo/", cors(corsOptions), todoRouter)
-app.options('*', cors())
 app.use("/users", cors(corsOptions), usersRouter)
 
 module.exports = app
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/userController")
+var cors = require('cors')
 
-router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", 
-               "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+
 
 router.route("/register").post(userController.registerUser)
-router.route("/login").get().post(userController.userLogin)
+router.options('/login', cors())
+router.route("/login", cors()).get().post(userController.userLogin)
 router.route("/logout").get(userController.userLogout)
 
 module.exports = router
