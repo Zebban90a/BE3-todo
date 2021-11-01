4dd80eb38e8736b5a6a7541fc320696b7a9ceaab
Sebastian Orbom <zebban90a@gmail.com>
2021-11-01 10:04:27 +0100

    corsss

--- server/app.js
+++ server/app.js
@@ -24,13 +24,7 @@ const app = express()
 
 app.use(cors(corsOptions)) */
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-  });
+app.use(cors())
 
 app.use(logger("dev"))
 app.use(express.json())
