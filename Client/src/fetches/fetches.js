b56238f87e5fadfd4924d0aa8907d8aa6d1be6c0
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-11-03 11:41:15 +0100

    Chnaged the url to point to Heroku urls

--- Client/src/fetches/fetches.js
+++ Client/src/fetches/fetches.js
@@ -1,10 +1,8 @@
 import axios from "axios"
 
-//axios.defaults.withCredentials = false;
-
 const url = axios.create({
 
-    baseURL: "http://localhost:4000/",
+    baseURL: "https://be3-todo.herokuapp.com/",
 })
 
 
